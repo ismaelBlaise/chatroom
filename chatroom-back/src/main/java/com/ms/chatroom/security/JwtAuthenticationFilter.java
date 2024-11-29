b0642dd6eb809584db145b035ac28aa1c24e8a58
ismaelBlaise <152376566+ismaelBlaise@users.noreply.github.com>
2024-11-29 15:15:55 +0300

    ok

---
+++ chatroom-back/src/main/java/com/ms/chatroom/security/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.ms.chatroom.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7); 
+            String username = jwtUtil.extractPseudo(token);
+
+            
+            if (username != null && jwtUtil.validateToken(token, username)) {
+                
+                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>()));
+            }
+        }
+
+        
+        filterChain.doFilter(request, response);
+    }
+}
