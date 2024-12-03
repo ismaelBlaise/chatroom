3e3b6d4d72e35fba1fad155db8ded4b772a87a96
ismaelBlaise <152376566+ismaelBlaise@users.noreply.github.com>
2024-12-03 08:34:28 +0300

    ok

--- chatroom-front/src/pages/Login.jsx
+++ chatroom-front/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import '../assets/Login.css';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -11,19 +12,14 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        
-        
       const response = await axios.post('http://localhost:8080/api/auth/login', { email, mdp: password });
       const { token } = response.data;
 
-      
       localStorage.setItem('authToken', token);
-
-      
       navigate('/dashboard');
     } catch (error) {
-      setError(error.response);
-    } 
+      setError(error.response?.data?.message || 'Une erreur s’est produite. Veuillez réessayer.');
+    }
   };
 
   return (
@@ -31,25 +27,27 @@ const Login = () => {
       <h2>Se connecter</h2>
       {error && <p className="error">{error}</p>}
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className="form-group">
           <label>Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            placeholder="Entrez votre email"
           />
         </div>
-        <div>
+        <div className="form-group">
           <label>Mot de passe</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            placeholder="Entrez votre mot de passe"
           />
         </div>
-        <button type="submit">Se connecter</button>
+        <button type="submit" className="login-button">Se connecter</button>
       </form>
     </div>
   );
